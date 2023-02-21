20487cfa45f38933a6b0d5bf777497873cc66ee1
Mauro Silva <silvac.mauro@gmail.com>
2023-02-21 17:37:18 -0300

    Correccion bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ImeiTextArea, ResultContainer } from './components';
 import { SaveDevices } from './components/SaveDevices';
 import { DisplayFilesName } from './components/DisplayFilesName';
 import { InvalidImeiContainer } from './components/InvalidImeiContainer';
+import { useForm } from './hook/useForm';
 
 const containerStyle = {
   padding: '10px',
@@ -9,13 +10,21 @@ const containerStyle = {
 }
 
 function App() {
+
+  const { onInputChange, fileName, setFormState } = useForm();
+
   return (
     <div style={ containerStyle }>
       <ImeiTextArea />
       <InvalidImeiContainer/>
       <ResultContainer />
-      <SaveDevices/>
-      <DisplayFilesName />
+      <SaveDevices
+        onInputChange={onInputChange}
+        fileName={fileName}
+      />
+      <DisplayFilesName 
+        setFormState={setFormState}
+      />
     </div>
   )
 }
--- src/components/InvalidIMEI.jsx
+++ src/components/InvalidIMEI.jsx
@@ -2,10 +2,27 @@ const listItemStyle = {
   margin: '5px 0'
 }
 
+const spanStyle = {
+  fontWeight: 'bold',
+}
+
 export const InvalidIMEI = ({device}) => {
   const {imei, model} = device;
+  const imeiLength = ('' + imei).length;
 
   return (
-    <li style={listItemStyle}>{`${imei} - ${model}`}</li>
+    <>
+      <li style={listItemStyle}>
+      {
+        `${imei} - ${model} `
+      }
+        <span style={spanStyle}>
+        {
+          `(${imeiLength} dígitos)`  
+        }
+        </span>
+      </li>
+      
+    </>
   )
 }
